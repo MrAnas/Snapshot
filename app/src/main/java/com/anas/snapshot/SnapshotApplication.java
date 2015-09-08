8a5cbc57f0b5e9261795e910b4b6308d38a8d9d1
Anas <anas2006101@gmail.com>
2015-09-08 16:49:31 +0300

    Creating Project

---
+++ app/src/main/java/com/anas/snapshot/SnapshotApplication.java
@@ -0,0 +1,16 @@
+package com.anas.snapshot;
+
+import android.app.Application;
+
+/**
+ * Created by Anas on 9/8/2015.
+ */
+public class SnapshotApplication extends Application {
+
+    void onCreate(){
+        super.onCreate();
+        
+
+
+    }
+}
